34ba3497bde5ca2775d7d74ecd7c7f36bfda0a18
Matsu <matsu3x9@icloud.com>
2026-06-13 11:36:44 +0800

    fish: trash

--- sh/fish/functions/trash.fish
+++ sh/fish/functions/trash.fish
@@ -1,33 +1,5 @@
 function trash
-    set -l TRASH_DIR "$HOME/.Trash"
 
-    # mkdir ~/.Trash dir
-    if not test -d $TRASH_DIR
-        # macOS don't do this
-        if string match -q "Darwin" (uname -s)
-            return 0
-        end
-        mkdir -p $TRASH_DIR
-    end
-
-    # clear
-    if test (count $argv) -eq 1
-        switch $argv[1]
-            case "-c"
-                # macOS don't do this
-                if string match -q "Darwin" (uname -s)
-                    return 0
-                end
-
-                echo "Clearing trash: $TRASH_DIR"
-                rm -rf $TRASH_DIR/*
-                echo "Trash cleared."
-
-                return 0
-        end
-    end
-
-    # no arg
     if test (count $argv) -eq 0
         echo "Usage:"
         echo "  trash <file1> <file2> ..."
@@ -35,14 +7,39 @@ function trash
         return 1
     end
 
-    # 逐个移动到废纸篓
+    if string match -q "Darwin" (uname -s)
+
+        if test (count $argv) -eq 1 -a $argv[1] = "-c"
+            osascript -e 'tell application "Finder" to empty the trash'
+            echo "Trash cleared."
+            return 0
+        end
+
+        # use system trash
+        command trash $argv
+        return $status
+    end
+
+    set -l TRASH_DIR "$HOME/.local/Trash"
+
+    if not test -d $TRASH_DIR
+        mkdir -p $TRASH_DIR
+    end
+
+    # clear
+    if test (count $argv) -eq 1 -a $argv[1] = "-c"
+        rm -rf $TRASH_DIR/*
+        echo "Trash cleared."
+        return 0
+    end
+
+    # mv
     for f in $argv
         if not test -e $f
             echo "trash: file not found: $f"
             continue
         end
 
-        # 防止重名
         set fname (basename $f)
         set ts (date "+%Y%m%d-%H%M%S")
         set target "$TRASH_DIR/$fname.$ts"
